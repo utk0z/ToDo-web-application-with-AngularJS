b6a9fa066e9c223fafb6a50b5d42f5a80bd144da
Semih Utku ÖZGAN <66949465+utk0z@users.noreply.github.com>
2023-02-23 15:28:43 +0300

    To-Do Web Application

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  
+  title: string ="To-Do's";
+  showAddTask: boolean= true;
+  subscription!: Subscription;
+
+  constructor(private uiService:UiService) {
+
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe((value) => (this.showAddTask = value));
+
+   }
+
+  ngOnInit(): void {
+  }
+  toggleAddTask(){
+    this.uiService.toggleAddTask();
+  }
+
+}
